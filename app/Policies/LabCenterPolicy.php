2ea35832534877f53fddb1feaacdb26aa6e78a01
Asad Ali Haider <itsasadalihaider@gmail.com>
2025-12-26 19:53:34 +0500

    generated policies

---
+++ app/Policies/LabCenterPolicy.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\LabCenter;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class LabCenterPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return $user->can('view_any_lab::center');
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, LabCenter $labCenter): bool
+    {
+        return $user->can('view_lab::center');
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $user->can('create_lab::center');
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, LabCenter $labCenter): bool
+    {
+        return $user->can('update_lab::center');
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, LabCenter $labCenter): bool
+    {
+        return $user->can('delete_lab::center');
+    }
+
+    /**
+     * Determine whether the user can bulk delete.
+     */
+    public function deleteAny(User $user): bool
+    {
+        return $user->can('delete_any_lab::center');
+    }
+
+    /**
+     * Determine whether the user can permanently delete.
+     */
+    public function forceDelete(User $user, LabCenter $labCenter): bool
+    {
+        return $user->can('force_delete_lab::center');
+    }
+
+    /**
+     * Determine whether the user can permanently bulk delete.
+     */
+    public function forceDeleteAny(User $user): bool
+    {
+        return $user->can('force_delete_any_lab::center');
+    }
+
+    /**
+     * Determine whether the user can restore.
+     */
+    public function restore(User $user, LabCenter $labCenter): bool
+    {
+        return $user->can('restore_lab::center');
+    }
+
+    /**
+     * Determine whether the user can bulk restore.
+     */
+    public function restoreAny(User $user): bool
+    {
+        return $user->can('restore_any_lab::center');
+    }
+
+    /**
+     * Determine whether the user can replicate.
+     */
+    public function replicate(User $user, LabCenter $labCenter): bool
+    {
+        return $user->can('replicate_lab::center');
+    }
+
+    /**
+     * Determine whether the user can reorder.
+     */
+    public function reorder(User $user): bool
+    {
+        return $user->can('reorder_lab::center');
+    }
+}
